8e1237ef4f16355d154740a3f170e64edd41c9bb
helenxiong12 <helenxiong12@gmail.com>
2016-05-13 01:15:01 +0900

    copied files over

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -27,25 +27,80 @@ public class WikiPhilosophy {
 	 * @param args
 	 * @throws IOException
 	 */
+
+
+
+
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
 
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
+		List<String> urlList = new ArrayList<String>();
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+		String goal = "https://en.wikipedia.org/wiki/Philosophy";
+		while (url.equals(goal) == false) {
+			Elements paragraphs = wf.fetchWikipedia(url);
+			boolean found = false;
+			//check for self-reference
+			urlList.add(url);
+			int num_open_parens = 0;
+			int num_close_parens = 0;
+			for (Element firstPara : paragraphs) {
+				Iterable<Node> iter = new WikiNodeIterable(firstPara);
+				boolean valid_link = false;
+				for (Node node: iter) {
+					if (node instanceof TextNode) {
+						TextNode tn = (TextNode) node;
+						String parens = tn.text();
+						num_open_parens += parens.length() - parens.replace("(", "").length();
+							num_close_parens += parens.length() - parens.replace(")", "").length();
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+						} else {
+							//is an element
+							Element e = (Element) node;
+							if (e.tagName() == "a") {
+								found = true;
+								//check for valid link
+								boolean has_italics = false;
+								Elements possible_italics = e.parents();
+								for (Element italics : possible_italics) {
+									if (italics.tagName().equals("i")) {
+										has_italics = true;
+									//System.out.println("italics is " + italics.toString());							
+										break;
+									}
+								}
+								//System.out.println("italics is " + italics.toString());							
+								String next_page = node.attr("href");
+								String next_page_url = "https://en.wikipedia.org" + next_page;
+								int end = next_page_url.indexOf('#');
+								boolean self_referential = false;
+								if (end != -1) {
+									self_referential = true;
+								}
+								if (urlList.contains(next_page_url)) {
+								//throw an error
+									self_referential = true;
+								}
+								if ((num_open_parens - num_close_parens)!=0 || (has_italics == true) || (self_referential == true)) {
+									found = false;
+								} else {
+									url = next_page_url;
+									break;
+								}
+							}
+						}	
+					}
+					//within article
+					if (found) break;
+				}
+				//finished article
+				if (found == false) {
+					System.out.println("Return false.");
+					break;
+				}
+
+			}
+			urlList.add(url);
+			System.out.println(urlList.toString());
+		}
 	}
-}
